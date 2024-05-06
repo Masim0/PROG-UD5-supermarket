a334f8fad6c42265aea7f0c5138ab46b82fb7563
masim0 <usr1693@salesianos-lacuesta.net>
2024-05-07 00:13:29 +0100

    feat: randomizer class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import com.salesianos.supermarket.utils.Randomizer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = Randomizer.getRandomName();
+        String product = Randomizer.getRandomProduct();
+
+        System.out.println(name);
+        System.out.println(product);
     }
 }
---
+++ src/com/salesianos/supermarket/utils/Randomizer.java
@@ -0,0 +1,17 @@
+package com.salesianos.supermarket.utils;
+
+import java.util.Random;
+
+public class Randomizer {
+    private static final String[] products = {"Manzana", "Harina", "Velas", "Libro", "Cafetera", "Camisa", "Televisor", "Zapatos", "Perfume", "Mesa", "Gafas", "Reloj"};
+    private static final String[] names = {"Juan", "María", "Pedro", "Ana", "Luis", "Carla", "Roberto", "Laura", "Sofía", "Javier", "Elena", "Diego", "Lucía", "Carlos", "Patricia", "Andrés", "Marta", "Ricardo", "Valentina", "Gabriel"};
+    private static final Random random = new Random();
+
+    public static String getRandomProduct() {
+        return products[random.nextInt(products.length)];
+    }
+
+    public static String getRandomName() {
+        return names[random.nextInt(names.length)];
+    }
+}
